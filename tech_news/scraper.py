e23d338059889352c0d846457e2e56ad728bfc38
Lucas da Silva <lucassouza00012345@gmail.com>
2023-04-13 18:35:44 -0300

    feat: implements scrape_news to return date about a post, create several functions to perfom

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -32,9 +32,59 @@ def scrape_next_page_link(html_content: str) -> str | None:
     return next_page["href"] if next_page else None
 
 
-# Requisito 4
-def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+def scrape_url(soup: BeautifulSoup) -> str | None:
+    url = soup.find("link", {"rel": "canonical"})
+    return url["href"] if url else None
+
+
+def scrape_title(soup: BeautifulSoup) -> str | None:
+    title = soup.find("h1", {"class": "entry-title"})
+    return title.text.rstrip() if title else None
+
+
+def scrape_timestamp(soup: BeautifulSoup) -> str | None:
+    timestamp = soup.find("li", {"class": "meta-date"})
+    return timestamp.text if timestamp else None
+
+
+def scrape_writer(soup: BeautifulSoup) -> str | None:
+    writer = soup.find("span", {"class": "author"})
+    return writer.a.text if writer else None
+
+
+def scrape_reading_time(soup: BeautifulSoup) -> int | None:
+    reading_time = soup.find("li", {"class": "meta-reading-time"})
+    if not reading_time:
+        return None
+
+    reading_time_number = "".join(filter(str.isdigit, reading_time.text))
+    return int(reading_time_number)
+
+
+def scrape_summary(soup: BeautifulSoup) -> str | None:
+    summary = soup.find("div", {"class": "entry-content"})
+    return summary.p.text.rstrip() if summary else None
+
+
+def scrape_category(soup: BeautifulSoup) -> str | None:
+    category = soup.find("div", {"class": "meta-category"}).a.find(
+        "span", {"class": "label"}
+    )
+    return category.text if category else None
+
+
+def scrape_news(html_content: str):
+    soup = BeautifulSoup(html_content, "html.parser")
+
+    return {
+        "url": scrape_url(soup),
+        "title": scrape_title(soup),
+        "timestamp": scrape_timestamp(soup),
+        "writer": scrape_writer(soup),
+        "reading_time": scrape_reading_time(soup),
+        "summary": scrape_summary(soup),
+        "category": scrape_category(soup),
+    }
 
 
 # Requisito 5
