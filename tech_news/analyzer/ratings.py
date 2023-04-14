8dde91337341616bf9af34e8212c30f161ab4d9d
Lucas da Silva <lucassouza00012345@gmail.com>
2023-04-14 17:53:49 -0300

    feat: implements top_5_categories function to return five categories more present in news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,12 @@
-# Requisito 10
+from tech_news.database import db
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    categories = db.news.aggregate(
+        [
+            {"$group": {"_id": "$category", "count": {"$sum": 1}}},
+            {"$sort": {"count": -1, "_id": 1}},
+            {"$limit": 5},
+        ]
+    )
+    return [category["_id"] for category in categories]
